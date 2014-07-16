c647e9c2ecbdbcdee3686e7eb920a71d698062bc
Huang Song <huangsong@gmail.com>
2014-07-16 22:58:54 +0800

    fix shop_favu logic

--- data-script/shop_favu.sh
+++ data-script/shop_favu.sh
@@ -3,11 +3,11 @@ source ./config.sh
 
 OUTPUT="../data/shop_favu.kv"
 SQL="
-select concat('U', user_id, '-FAVS'), group_concat(shop_id separator ';') as sids
+select concat('S', shop_id, '-FAVU'), group_concat(user_id separator ';') as uids
 from taobao_shop_user su
      join taobao_seller_info seller on seller.taobao_seller_id=su.shop_id
 where shop_id <> 1 and seller.taobao_block=0
-group by user_id;
+group by shop_id;
 "
 
 $MYSQL -N -h $HOST -D $DB --password=$PASS $OTHER -e "$SQL" > $OUTPUT
