cfc0bf7516bca8f34e55648d678353001f6b73ec
Huang Song <huangsong@gmail.com>
2014-07-19 16:00:34 +0800

    add runall.sh

---
+++ data-script/runall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for script in dict_shop.sh dict_user.sh goods_binfo.sh goods_price.sh goods_sales.sh shop_binfo.sh shop_favu.sh shop_goods.sh user_actg.sh user_acts.sh user_favg.sh user_favs.sh;
+do
+	echo $script
+	time . ./$script
+done
+	
