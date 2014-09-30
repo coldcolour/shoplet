e2854ade9186e71ce62003523c8815b7997b9826
Huang Song <huangsong@gmail.com>
2014-09-30 15:39:22 +0800

    index first, before classification

--- data-script/runall.sh
+++ data-script/runall.sh
@@ -19,6 +19,10 @@ echo goods_txt
 cat ../data/goods_name.csv ../data/goods_prop_value.csv ../data/goods_rate.csv ../data/goods_tag.csv > ../data/goods_txt
 sort -k1,1n -o ../data/goods_txt ../data/goods_txt 
 pypy merge_lines.py ../data/goods_txt ../data/foo && mv ../data/foo ../data/goods_txt
+
+echo index
+time python ../../pywvtool/pywvtool.py -t goods -o ../data/goods_index --loader="LocalKVFileLoader" --loader-op="src=../data/goods_txt" --tokenizer="MMSegTokenizer" -w TFIDF
+
 echo classify
 time pypy ../mining/classify2.py ../data/goods_name.csv ../data/cat.words.csv > ../data/goods_category.csv
 gawk -F'\t' '{printf("G%s-CAT\t%s\n",$1,$3)}' ../data/goods_category.csv | gawk -F "\t" 's != $1 || NR ==1{s=$1;if(p){print p};p=$0;next}{sub($1,"",$0);p=p""$0;}END{print p}' | sed 's/	/;/g;s/;/	/1' > ../data/goods_cat.kv
@@ -29,14 +33,16 @@ gawk -F',' '{print $1}' ../data/cat.words.csv | cat -n | sed 's/ //g;s/\t/ /g' |
 join -j 2 ../data/goods.group.1 ../data/goods.group.2 | gawk '{printf "%d,%d\n",$3,$2}' | sort -t, -k2,2 > ../data/goods.group.3
 join -t, -j 2 ../data/goods_index/goods.docinfo ../data/goods.group.3 | awk -F, '{printf "%d\t%d\n",$2,$3}' > ../data/goods.group
 rm ../data/goods.group.*
-echo index
-time python ../../pywvtool/pywvtool.py -t goods -o ../data/goods_index --loader="LocalKVFileLoader" --loader-op="src=../data/goods_txt" --tokenizer="MMSegTokenizer" -w TFIDF
-# echo simi
+
+echo simi
 cd ../mining
 time pypy mpsmsimi.py ../data/goods_index/goods.wv ../data/goods.simi ../data/goods.group
-# filter
+
+echo filter
 time pypy filter_simi.py ../data/goods.simi ../data/goods_index/goods.docinfo ../data/goods_name.csv ../data/name.me ../data/goods.filtered.simi
-# sample
+
+echo sample
 shuf -n 50 ../data/goods.filtered.simi > ../data/goods.simi.sample
-# sample html page
+
+echo sample html page
 python goods_simi_demo.py ../data/goods.simi.sample ../data/goods_index/goods.docinfo ../data/goods.sample.html
